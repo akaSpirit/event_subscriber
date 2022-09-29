e3ff265caa3e18ba8dc017aafe555ac620a45658
Spirit <mcspiritkg@gmail.com>
2022-09-29 10:30:06 +0600

    created project

---
+++ src/test/java/com/example/event_subscriber/EventSubscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.event_subscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSubscriberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
